42dfe467e742c01b252bf92d45c33912d35dbed6
RemasAllam <msabader581@gmail.com>
2026-04-16 17:23:47 +0200

    Loaded & Cleaned Data & Binarized 'Status' Column

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,16 +1,17 @@
 import pandas as pd
 import numpy as np
 
-# Import URL dataset
-Dataset = pd.read_excel('C:/Users/remas/Documents/UNI/Level 4 Primers/Phishing Detector/Project/Data/Processed Dataset.xlsx')
 
-df = pd.DataFrame(Dataset)
+def import_data(filepath):
+    # Import URL dataset
+    return pd.read_excel(filepath)
 
-# Removing Missing and Null Values
-df.dropna(inplace=True)
+def clean_data(df):
+    # Removing Missing and Null Values
+    df.dropna(inplace=True)
 
-# Removing Duplicates
-df.drop_duplicates(inplace=True)
+    # Removing Duplicates
+    df.drop_duplicates(inplace=True)
 
-# Binarizing the status column (phishing = 1, legitimate = 0)
-df['status'] = np.where(df['status'] == 'phishing', 1, 0)
+    # Binarizing the status column (phishing = 1, legitimate = 0)
+    df['status'] = np.where(df['status'] == 'phishing', 1, 0)
