18fef9625822f4dff3dd5ec8db2ee79f72704e49
RemasAllam <msabader581@gmail.com>
2026-04-17 10:05:18 +0200

    Defined the 5 Models

---
+++ src/models.py
@@ -0,0 +1,15 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
+from xgboost import XGBClassifier
+
+def models_definition():
+
+    return {
+        'Random Forest': RandomForestClassifier(n_estimators=100, random_state=42),
+        'XGBoost': XGBClassifier(eval_metric='logloss', random_state=42),
+        'SVM': SVC(kernel='rbf', probability=True, random_state=42),
+        'Naive Bayes': GaussianNB(),
+        'Neural Network': MLPClassifier(hidden_layer_sizes=(100, 50), max_iter=500, activation='relu', solver='adam', random_state=42)
+    }
